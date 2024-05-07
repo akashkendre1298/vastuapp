7d0c6b90304eb71ad686ea3dcdbf216524e74155
akashkendre1298 <kendreakash34@gmail.com>
2024-05-07 16:28:42 +0530

    update SignUp.tsx

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom"; // Import useHistory hook for navigation
+import { useHistory } from "react-router-dom";
 import { IonPage, IonImg } from "@ionic/react";
 import "./SignUp.css";
 import logo from "../../Assets/pandit_shivkumar_logo.png";
@@ -11,12 +11,24 @@ const SignupPage = () => {
   const [phonenumber, setPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const [successMessage, setSuccessMessage] = useState(""); // State variable for success message
-
-  const history = useHistory(); // Initialize useHistory
+  const [successMessage, setSuccessMessage] = useState("");
+  const history = useHistory();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // Check if any field is empty
+    if (!firstname || !lastname || !email || !phonenumber || !password) {
+      setError("All fields are mandatory.");
+      return;
+    }
+
+    // Validate phone number
+    const phoneRegex = /^[1-9]\d{9}$/;
+    if (!phoneRegex.test(phonenumber)) {
+      setError("Invalid phone number. Please enter a 10-digit number.");
+      return;
+    }
 
     const userData = {
       firstname,
@@ -37,7 +49,6 @@ const SignupPage = () => {
 
       if (response.ok) {
         console.log("User signed up successfully");
-        // Clear form fields after successful signup
         setFirstName("");
         setLastName("");
         setEmail("");
@@ -48,99 +59,94 @@ const SignupPage = () => {
         );
         setTimeout(() => {
           setSuccessMessage("");
-          history.push("/"); // Redirect to login page
-        }, 3000); // Redirect after 3 seconds
+          history.push("/");
+        }, 3000);
       } else {
         setError("Failed to sign up user. Please try again.");
       }
     } catch (error) {
       console.error("Error:", error);
-
       setError("Network error. Please check your internet connection.");
     }
   };
 
   return (
-    <>
-      <IonPage>
-        <div className="signup-page">
-          <div className="signupdiv-htmlhtmlFor-logo">
-            <IonImg src={logo} className="signup-logo"></IonImg>
-          </div>
-          <div className="signup-heading">
-            <p>Sign Up</p>
-            <p className="para-after-signup-heading">
-              Please enter details to create account
-            </p>
-          </div>
-          <div className="input-div">
-            <label htmlFor="" className="label-signup">
-              First Name
-            </label>
-            <input
-              type="text"
-              placeholder="First Name"
-              className="input-signup"
-              value={firstname}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <label htmlFor="lastname" className="label-signup">
-              Last Name
-            </label>
-            <input
-              type="text"
-              id="lastname"
-              placeholder="Last Name"
-              className="input-signup"
-              value={lastname}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            <label htmlFor="email" className="label-signup">
-              Email Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              className="input-signup"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="phonenumber" className="label-signup">
-              Contact Number
-            </label>
-            <input
-              type="tel"
-              id="phonenumber"
-              placeholder="Phone number"
-              className="input-signup"
-              value={phonenumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-            <label htmlFor="password" className="label-signup">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Create password"
-              className="input-signup"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />{" "}
-          </div>
-          {error && <div className="error-message">{error}</div>}
-          {successMessage && (
-            <div className="success-message">{successMessage}</div>
-          )}
-          <div>
-            <button className="signUp-button" onClick={handleSubmit}>
-              Create Account
-            </button>
-          </div>
+    <IonPage>
+      <div className="signup-page">
+        <div className="signupdiv-htmlhtmlFor-logo">
+          <IonImg src={logo} className="signup-logo" />
         </div>
-      </IonPage>
-    </>
+        <div className="signup-heading">
+          <p>Sign Up</p>
+          <p className="para-after-signup-heading">
+            Please enter details to create account
+          </p>
+        </div>
+        <div className="input-div">
+          <label htmlFor="" className="label-signup">
+            First Name
+          </label>
+          <input
+            type="text"
+            placeholder="First Name"
+            className="input-signup"
+            value={firstname}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <label htmlFor="lastname" className="label-signup">
+            Last Name
+          </label>
+          <input
+            type="text"
+            id="lastname"
+            placeholder="Last Name"
+            className="input-signup"
+            value={lastname}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <label htmlFor="email" className="label-signup">
+            Email Address
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            className="input-signup"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="phonenumber" className="label-signup">
+            Contact Number
+          </label>
+          <input
+            type="tel"
+            id="phonenumber"
+            placeholder="Phone number"
+            className="input-signup"
+            value={phonenumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <label htmlFor="password" className="label-signup">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Create password"
+            className="input-signup"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {error && <div className="error-message">{error}</div>}
+        {successMessage && <div className="success-message">{successMessage}</div>}
+        <div>
+          <button className="signUp-button" onClick={handleSubmit}>
+            Create Account
+          </button>
+        </div>
+      </div>
+    </IonPage>
   );
 };
 
